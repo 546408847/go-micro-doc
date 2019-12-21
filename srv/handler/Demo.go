af0538f303d968555dd3be5bc42a684a680d43f9
zhouliang <546408847@qq.com>
2019-12-21 10:01:03 +0800

    fix

--- srv/handler/Demo.go
+++ srv/handler/Demo.go
@@ -11,6 +11,6 @@ type Demo struct {
 
 func (d *Demo) Hello(ctx context.Context, req *go_micro_demo.NameRequest, resp *go_micro_demo.HelloResponse) error{
 	//todo db curd
-	resp.Content = req.Name + "欢迎你，成功调用rpc"
+	resp.Content = req.Name + "!欢迎你，成功调用rpc"
 	return nil
 }
