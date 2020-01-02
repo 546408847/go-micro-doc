1765acbad742b29303b4f78afb92f7d2d26d04cc
zhouliang <546408847@qq.com>
2020-01-02 19:16:01 +0800

    fix

--- srv/handler/Demo.go
+++ srv/handler/Demo.go
@@ -2,14 +2,14 @@ package handler
 
 import (
 	"context"
-	gomicrodemo "go-micro-doc/proto/demo"
+	goMicroDemo "go-micro-doc/proto/demo"
 )
 
 type Demo struct {
 
 }
 
-func (d *Demo) Hello(ctx context.Context, req *gomicrodemo.NameRequest, resp *gomicrodemo.HelloResponse) error{
+func (d *Demo) Hello(ctx context.Context, req *goMicroDemo.NameRequest, resp *goMicroDemo.HelloResponse) error{
 	//todo db curd
 	resp.Content = req.Name + "!欢迎你，成功调用rpc"
 	return nil
